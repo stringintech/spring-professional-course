e4ee5a015319911f19880e5563c74c887e49109b
stringintech <stringintech@gmail.com>
2024-09-18 21:59:11 +0330

    Springify app context

--- mybank/src/main/java/com/stringintech/mybank/context/Context.java
+++ mybank/src/main/java/com/stringintech/mybank/context/Context.java
@@ -3,12 +3,18 @@ package com.stringintech.mybank.context;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.stringintech.mybank.service.TransactionService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Context {
-    public final static TransactionService transactionService = new TransactionService();
-    public final static ObjectMapper objectMapper = new ObjectMapper();
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
+    }
 
-    static {
-        objectMapper.registerModule(new JavaTimeModule());
+    @Bean
+    public TransactionService getTransactionService() {
+        return new TransactionService();
     }
 }
--- mybank/src/main/java/com/stringintech/mybank/web/BankServlet.java
+++ mybank/src/main/java/com/stringintech/mybank/web/BankServlet.java
@@ -1,16 +1,30 @@
 package com.stringintech.mybank.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stringintech.mybank.context.Context;
 import com.stringintech.mybank.model.Transaction;
+import com.stringintech.mybank.service.TransactionService;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
 public class BankServlet extends HttpServlet {
+
+    private TransactionService transactionService;
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void init() throws ServletException {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Context.class);
+        this.transactionService = ctx.getBean(TransactionService.class);
+        this.objectMapper = ctx.getBean(ObjectMapper.class);
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -19,10 +33,10 @@ public class BankServlet extends HttpServlet {
             Integer amount = Integer.valueOf(request.getParameter("amount"));
             String reference = request.getParameter("reference");
 
-            Transaction tx = Context.transactionService.create(amount, reference);
+            Transaction tx = this.transactionService.create(amount, reference);
 
             response.setContentType("application/json; charset=UTF-8");
-            String json = Context.objectMapper.writeValueAsString(tx);
+            String json = this.objectMapper.writeValueAsString(tx);
             response.getWriter().print(json);
         } else {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -33,8 +47,8 @@ public class BankServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         if (request.getRequestURI().equalsIgnoreCase("/transactions")) {
             response.setContentType("application/json; charset=UTF-8");
-            List<Transaction> txs = Context.transactionService.getAll();
-            response.getWriter().print(Context.objectMapper.writeValueAsString(txs));
+            List<Transaction> txs = this.transactionService.getAll();
+            response.getWriter().print(this.objectMapper.writeValueAsString(txs));
         } else {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
